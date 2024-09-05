f64e444fd8495d58e41d76752ef54e9996a2fb31
Như Như <nhuyu674@gmail.com>
2024-09-05 21:25:34 +0700

    baivenha

---
+++ BaiTapVeNha_20/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha_20.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Hoten = "Lê Thị Quỳnh Như";
+            ViewBag.MSSV = "1822041437";
+            ViewData["Nam"] = "2004";
+            return View();
+        }
+    }
+}
